69f456554912b0c67a7f4d8a19a1a1d106ea2f8b
Jessica Tam <jessica.tam128@gmail.com>
2023-10-08 20:37:40 -0400

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
